5176cb35ac84931e1b21025e35c289633e943f5c
Mr-Saransh <saranshagrahari1221@gmail.com>
2026-07-13 19:55:32 +0530

    clerk problem

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   serverExternalPackages: ["better-sqlite3"],
+  env: {
+    NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY: process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY || "pk_test_ZmxleGlibGUtcHJpbWF0ZS00LmNsZXJrLmFjY291bnRzLmRldiQ",
+  },
 };
 
 export default nextConfig;
